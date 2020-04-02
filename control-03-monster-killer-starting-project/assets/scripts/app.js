10b480f3fa64f9b9f0e18106b9a772880604599d
Sandra Coburn <sandra_coburn@hotmail.com>
2020-04-02 11:05:25 -0700

    Adding global constants as identifiers in conditional code

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,11 +1,18 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
-let chosenMaxLife = 100;
+const HEAL_VALUE = 20;
+
+const enteredValue = prompt("Maximun life for you and the monster", "100");
+let chosenMaxLife = parseInt(enteredValue);
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
 
 function attackHandler() {
   attackMonster("ATTACK");
@@ -29,12 +36,30 @@ function attackMonster(mode) {
   }
   const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
+  endRound();
+  //   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  //   currentPlayerHealth -= playerDamage;
+  //   if (currentMonsterHealth <= 0 && currentPlayerHealth >= 0) {
+  //     alert("You won!");
+  //   } else if (currentPlayerHealth <= 0 && currentMonsterHealth >= 0) {
+  //     alert("you lost!");
+  //   } else {
+  //       alert("you have a draw")
+  //   }
+}
+
+function endRound() {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
   if (currentMonsterHealth <= 0 && currentPlayerHealth >= 0) {
     alert("You won!");
+    reset();
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth >= 0) {
     alert("you lost!");
+    reset();
+  } else {
+    alert("you have a draw");
+    reset();
   }
 }
 
@@ -50,6 +75,17 @@ function strongAttackHandler() {
   //    alert("you lost!");
   //  }
 }
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+function healPlayerHandler() {
+  increasePlayerHealth(HEAL_VALUE);
+  currentPlayerHealth += HEAL_VALUE;
+  endRound();
+}
 
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
