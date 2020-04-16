560a23bf4086671443be7463da1dcbeb39985367
Sandra Coburn <sandra_coburn@hotmail.com>
2020-04-16 14:27:43 -0700

    function samples

--- functions-04-game-user-choice/app.js
+++ functions-04-game-user-choice/app.js
@@ -76,9 +76,12 @@ startGameBtn.addEventListener("click", function () {
 
 // not related to the game
 const sumUp = (a, b, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
   let sum = 0;
   for (const num of numbers) {
-    sum += num;
+    sum += validateNumber(num);
   }
   return sum;
 };
