96752b0f6f6ae3ed666564e06d00c95a8946fef8
Sandra Coburn <sandra_coburn@hotmail.com>
2020-04-26 20:27:56 -0700

    The this word in an object

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -25,11 +25,17 @@ const renderMovies = (filter = "") => {
     const movieEl = document.createElement("li");
     // movieEl.textContent = movie.info.title;
 
+    //Checking for property existence. if info is found in movie
+//     if(!("info" in movie)) {
+// //write code here
+//     }
+
     //Object destructuring. We can use the key info so we dont have to write movie.info on the following lines
     const { info, ...otherProps } = movie; //otherProps has all the properties not passed by name
     console.log("other props", otherProps);
+    if(info)
     // let text = movie.info.title + " - ";
-    let text = info.title + " - ";
+    let text = info.title.toUpperCase() + " - ";
     // for (const key in movie.info) {
     for (const key in info) {
       if (key !== "title") {
@@ -64,6 +70,9 @@ const addMovieHandler = () => {
       [extraName]: extraValue, //extraName is a variable that will hold the extraValue value
     },
     id: Math.random().toString(),
+    getFormattedTitle: function() {
+        return this.info.title.toLowerCase()//it will use this word to specify the object where the function was called
+    }
   };
   movies.push(newMovie);
   console.log(newMovie);
